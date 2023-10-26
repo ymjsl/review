e247185e7256b4afc9f0b785cc37d6ca779d4ab8
ymjsl <141890844+ymjsl@users.noreply.github.com>
2023-10-26 12:49:21 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,148 +1,99 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-//文本文件和二进制文件
-//以二进制存储占据内存可能会更小
+#include<string.h>
+#include<math.h>
+//程序员的自我修养 c陷阱和缺陷
+//预定义符号
+//__FILE__
+//__LINE__
+//__TIME__
+//__DATE__
 //int main()
 //{
-//	int a = 10000;
-//	FILE* pf = fopen("C:\\Users\\admin\\Desktop\\cnm.txt.txt", "wb");
-//	fwrite(&a, 4, 1, pf);
+//	int i = 0;
+//	FILE* pf = fopen("C:\\Users\\admin\\Desktop\\cnm.txt.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		fprintf(pf,"file:%s line=%d date:%s i=%d\n",\
+//          __FILE__,\
+//          __LINE__,\
+//          __DATE__,i);
+//	}
 //	fclose(pf);
 //	pf = NULL;
 //	return 0;
 //}
- //feof 应用于当文件读取结束时，为真 读到末尾
- // 判断读取失败结束还时遇到文件尾结束
- // ferror 判断读取是否错误   为真   读取错误     配合使用
-//判断文件是否结束看函数返回值
-//fgetc  EOF
-//fgets  NULL
-//fread  返回的是实际读到的个数，比较个数
-//文件缓冲区
-//输入缓冲区  输出缓冲区
-//sleep() 单位毫秒
-//程序环境和预处理
-//翻译环境  执行环境
-//链接器 link.exe
-//编译器 cl.exe 
-// 编译（预编译/预处理           编译             汇编）    链接
-//  生成新的文件在新文件中   将c转换成汇编代码  gcc test.s-c
-//   头文件包含 ，define        语法分析        生成test.o
- //    定义符号替换             词法分析        目标文件
-//      符号删除                符号汇总      （全是二进制指令）
-//     删除注释                 语义分析       形成符号表
-//                            生成test.s        
+//define 定义标识符
+//#define MAX 1000
+//#define STR "hello bit"
+//#define print printf("hehe\n")
 //int main()
 //{
-//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int i = 0;
-//	for (i = 0; i < 10; i++)
-//	{
-//		printf("%d ", arr[i]);
-//	}
+//	int m = MAX;
+//	printf("%d\n", MAX);
+//	printf("%s\n", STR);
+//	print;
 //	return 0;
 //}
-//struct S
-//{
-//	int count;
-//	int arr[];
-//};
-//struct num
-//{
-//	struct S* s1;
-//};
-//int cmp_stu_by_arr(const void* e1, const void* e2)
-//{
-//	return  ((struct num*)e1)->s1->arr - ((struct num*)e2)->s1->arr;
-//}
-//int main()
-//{
-//	int n = 0;
-//	while (scanf("%d", &n) == 1)
-//	{
-//		struct num* p1 = (struct num*)malloc(4 * n);
-//		for (int i = 0; i < n; i++)
-//		{
-//			scanf("%d", &(p1->arr[i]));
-//			
-//		}
-//		qsort(p1, sizeof(struct S), n, cmp_stu_by_arr);
-//		for (int j = 0; j < n; j++)
-//		{
-//			printf("%d ", p1->count);
-//		}
-//		return 0;
-//	}
-//}
-
-//#include <stdio.h>
+//#define CASE break;case
 //
 //int main()
 //{
-//	int i, j;
-//	int a;
-//	scanf("%d", &a);
-//	int arr[100];
-//	int brr[100];
-//	for (i = 0; i < a; i++) {
-//		scanf("%d", &arr[i]);
+//	switch ()
+//	{
+//	case 1:
+//	CASE 2:
+//	CASE 3:
+//	CASE 4:
 //	}
-//	for (i = 0; i < a; i++) {
-//		brr[i] = arr[i];
-//	}
-//	for (i = 0; i < a -1; i++) {
-//		for (j =i; j < a ; j++) {
-//			if (arr[j] < arr[j + 1]) {
-//				int t = arr[j];
-//				arr[j] = arr[j + 1];
-//				arr[j + 1] = t;
-//			}
-//		}
-//	}for (i = 0; i < a; i++) {
-//		for (j = 0; j < a; j++) {
-//			if (arr[i] == brr[j]) {
-//				printf("%d ", j + 1);
-//				break;
-//			}
-//		}
-//	}
-//
 //	return 0;
 //}
-#include <stdio.h>
-
-int main() {
-	int i, j;
-	int a;
-	scanf("%d", &a);
-	int arr[100];
-	int brr[100];
-	for (i = 0; i < a; i++) {
-		scanf("%d", &arr[i]);
-	}
-	for (i = 0; i < a; i++) {
-		brr[i] = arr[i];
-	}
-
-	for (i = 0; i < a - 1; i++) {
-		for (j = 0; j < a - 1 - i; j++) {
-			if (arr[j] > arr[j + 1]) {
-				int t = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = t;
-			}
-		}
-	}
-	for (i = 0; i < a; i++) {
-		for (j = 0; j < a; j++) {
-			if (arr[i] == brr[j]) {
-				printf("%d ", j + 1);
-				break;
-			}
-		}
-	}
-
+//#define 定义宏    不要吝啬括号！！
+//#define name() stuff
+//#define SQUARE(x) pow(x,2)
+//// name 必须与(紧邻
+//int main()
+//{
+//	int r = SQUARE(5+1);//1
+//	//只替换不计算参数
+//	//int r=5+1*5+1=11
+//	printf("%d\n", r);
+//	return 0;
+//}
+//#define DOUBLE(x) ((x)+(x))
+//int main()
+//{
+//	int r = DOUBLE(3*2);
+//	printf("%d\n",r);
+//	return 0;
+//}
+//替换规则
+//首先替换define 定义的
+//然后替换宏   只替换不计算参数
+//宏不能递归
+//字符串里的字符不会被替换
+//      #      和       ##
+//将参数插入字符串
+//void print(int n)
+//{
+//	printf("the value of n %d\n", n);
+//}
+#define PRINT(n) printf("the value of "#n" is %d\n",n)
+int main()
+{
+	/*printf("hello world\n");
+	printf("hello ""world\n");*/
+	int a = 10;
+	int b = 20;
+	/*printf("the value of a is %d\n", a);
+	printf("the value of b is %d\n", b);*/
+	PRINT(a);
+	PRINT(b);
 	return 0;
 }
